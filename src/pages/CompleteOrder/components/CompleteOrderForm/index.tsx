ffa127e206c74f70473b612c6834c031a5c12555
Fernando Souza <l.fernando.dev@gmail.com>
2023-01-25 23:04:39 -0300

    Create complete order page

--- src/pages/CompleteOrder/components/CompleteOrderForm/index.tsx
+++ src/pages/CompleteOrder/components/CompleteOrderForm/index.tsx
@@ -1,14 +1,23 @@
 import { TitleText } from "../../../../components/Typography";
+import { SectionTitle } from "../SectionTitle";
 import { CompletOrderFormContainer, FormSectionContainer } from "./styles";
+import { MapPinLine } from "phosphor-react";
+import { useTheme } from "styled-components";
 
 export function CompletOrderForm () {
+    const { colors } = useTheme()
+
     return (
         <CompletOrderFormContainer>
             <TitleText size='xs' color="subtitle">
                 Complete seu pedido
             </TitleText>
             <FormSectionContainer >
-                <div>Teste</div>
+                <SectionTitle
+                    title="Endereço de Entrega"
+                    subtitle="Informe o endereço onde deseja receber o pedido"
+                    icon={<MapPinLine size={22} color={colors['brand-yellow-dark']} />}
+                />
             </FormSectionContainer>
         </CompletOrderFormContainer>
     )
