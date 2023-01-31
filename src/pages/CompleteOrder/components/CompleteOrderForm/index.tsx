d938f80204c920be759a6b3513dd7f5e2c9d22c9
Fernando Souza <l.fernando.dev@gmail.com>
2023-01-30 22:42:58 -0300

    create section input complete order page

--- src/pages/CompleteOrder/components/CompleteOrderForm/index.tsx
+++ src/pages/CompleteOrder/components/CompleteOrderForm/index.tsx
@@ -1,8 +1,9 @@
 import { TitleText } from "../../../../components/Typography";
 import { SectionTitle } from "../SectionTitle";
 import { CompletOrderFormContainer, FormSectionContainer } from "./styles";
-import { MapPinLine } from "phosphor-react";
+import { MapPinLine, CurrencyDollar } from "phosphor-react";
 import { useTheme } from "styled-components";
+import { AddressForm } from "./AddressForm";
 
 export function CompletOrderForm () {
     const { colors } = useTheme()
@@ -18,6 +19,15 @@ export function CompletOrderForm () {
                     subtitle="Informe o endereço onde deseja receber o pedido"
                     icon={<MapPinLine size={22} color={colors['brand-yellow-dark']} />}
                 />
+                <AddressForm/>
+            </FormSectionContainer>
+            <FormSectionContainer >
+                <SectionTitle
+                    title="Pagamento"
+                    subtitle="O pagamento é feito na entrega. Escolha a forma que deseja pagar"
+                    icon={<CurrencyDollar size={22} color={colors['brand-purple']} />}
+                />
+                
             </FormSectionContainer>
         </CompletOrderFormContainer>
     )
---
+++ src/pages/CompleteOrder/components/PaymentMethodInput/index.tsx
@@ -0,0 +1,6 @@
+
+export const PaymentMethodInput = () => {
+  return (
+    <div>index</div>
+  )
+}
